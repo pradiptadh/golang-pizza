485dae3e3cc275c7b6cd0b18583ff9be3c9ca699
pradiptadh <haryadipradipta@gmail.com>
2020-12-11 16:27:57 +0700

    second commit

--- main.go
+++ main.go
@@ -49,6 +49,6 @@ func main() {
 	e.DELETE("/hapus_menu", handler.HapusData)
 	e.Static("/static", "assets")
 	e.POST("/tambah_order", handler.InputOrder)
-	// Start the Echo server
+	// Start the Echo server w ganteng
 	e.Logger.Fatal(e.Start(":1323"))
 }
